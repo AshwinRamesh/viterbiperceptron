cc7acf5f22e87f0cd1ebe028b56da56751a9beb1
Ashwin Ramesh <ashramesh1992@gmail.com>
2014-06-06 13:03:09 +1000

    data +test suite

---
+++ tests/run_suite.py
@@ -0,0 +1,54 @@
+from viterbiperceptron.perceptron import FeaturePerceptronOne, FeaturePerceptronTwo, Sentence
+from viterbiperceptron.data_parser import read_file, load_perceptron_from_file, write_output_to_file
+import json
+
+# Set of all perceptron subclasses to run
+suites = {
+            1: FeaturePerceptronOne,
+            2: FeaturePerceptronTwo
+        }
+
+# Defines which suites to actually run
+run_suites = [1]
+
+
+# Read the file into documents
+docs = read_file("../data/conll03/eng.train")
+training_data = []
+for doc in docs:
+    for sentence in doc:
+        training_data.append(Sentence.create_sentence_from_list(sentence))
+
+docs = read_file("/Users/Ash/Documents/University/comp5046/assignments/assignment3/data/conll03/eng.testa")
+testa_data = []
+for doc in docs:
+    for sentence in doc:
+        testa_data.append(Sentence.create_sentence_from_list(sentence))
+
+docs = read_file("/Users/Ash/Documents/University/comp5046/assignments/assignment3/data/conll03/eng.testb")
+testb_data = []
+for doc in docs:
+    for sentence in doc:
+        testb_data.append(Sentence.create_sentence_from_list(sentence))
+
+print "Processed all data.. iterating now.."
+
+for suite in run_suites:
+    perceptron = suites[suite](training_data=training_data, iterations=3)
+
+    print "Beginning Training"
+    perceptron.train()
+    f = file("eng.train.out.suite%d" % suite, "w+")
+    f.write(json.dumps(perceptron.weights))
+    f.close()
+    print "Processed Training Data"
+
+    for sentence in testa_data:
+        perceptron.classify(sentence)
+    write_output_to_file(testa_data, "eng.testa.out")
+
+    for sentence in testb_data:
+        perceptron.classify(sentence)
+    write_output_to_file(testa_data, "eng.testb.out")
+
+print "Done testing."
